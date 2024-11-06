b77c21857c710c6982113f3c1c20164e8234d4a3
debjyoti ghosh <debjyoti1ghosh@gmail.com>
2024-11-06 22:45:46 +0530

    new

--- actual_value.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Use your Polygon.io API key
-url = 'https://api.polygon.io/v3/reference/tickers?apiKey=pVRpwGkbyVr28GAuYVr0alz1dv4gZRER'
-r = requests.get(url)
-data = r.json()
-
-# This will give a list of all stock symbols
-print(data)
